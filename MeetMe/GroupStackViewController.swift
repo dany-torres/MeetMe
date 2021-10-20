643e4979827025400e606d231bd5d6d10bfc7c44
Pamela Vazquez <pamvazquez1@gmail.com>
2021-10-19 19:14:09 -0500

    Adding Create Event and Group Stack Code

--- MeetMe/GroupStackViewController.swift
+++ MeetMe/GroupStackViewController.swift
@@ -7,23 +7,53 @@
 
 import UIKit
 
-class GroupStackViewController: UIViewController {
 
+protocol AddNewEvent {
+    func addNewEvent(newEvent: Event)
+}
+
+class GroupStackViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AddNewEvent {
+    public var eventList:[Event] = []
+    var delegate: UITableView!
+    
+    @IBOutlet weak var eventStack: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        eventStack.delegate = self
+        eventStack.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        eventList.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "stackCell", for: indexPath)
+        
+        //After creating the cell, update the properties of the cell with appropriate data values.
+        let row = indexPath.row
+        cell.textLabel?.text = eventList[row].printEventDetails()
+        return cell
+    }
+    
 
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         if segue.identifier == "CreateEventSegue",
+            let nextVC = segue.destination as? CreateEventViewController {
+             nextVC.delegate = self
+         
+         }
+     }
+    
+    func addNewEvent(newEvent: Event) {
+            eventList.append(newEvent)
+            eventStack.reloadData()
+        }
+         
 }
