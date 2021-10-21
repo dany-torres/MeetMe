3be9a4fc32070cf73d953bd1b907a213b5fe6786
Rodrigo Estrella <restrella@outlook.com>
2021-10-21 18:38:14 -0500

    added sign in, sign up and logout functionality

--- MeetMe/ViewController.swift
+++ MeetMe/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // NOTE: Used for testing logging in user
+//        do {
+//            try Auth.auth().signOut()
+//        } catch let signOutError as NSError {
+//          print("Error signing out: %@", signOutError)
+//        }
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if user != nil{
+                self.performSegue(withIdentifier: "LoginSegue", sender: nil)
+                self.usernameTextField.text = nil
+                self.passwordTextField.text = nil
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +36,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
+        guard let email = usernameTextField.text,
+              let password = passwordTextField.text,
+              email.count > 0,
+              password.count > 0
+        else {
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) {
+          user, error in
+          if let error = error, user == nil {
+            let alert = UIAlertController(
+              title: "Sign in failed",
+              message: error.localizedDescription,
+              preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title:"OK",style:.default))
+            self.present(alert, animated: true, completion: nil)
+          } else {
+//              self.validLogin = true
+          }
+        }
     }
 }
 
