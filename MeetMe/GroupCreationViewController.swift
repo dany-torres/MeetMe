1afcc7fd7db81f7c80a4f43cf1ecae410e6d85a0
Rodrigo Estrella <restrella@outlook.com>
2021-11-04 01:18:32 -0500

    added group instance on database

--- MeetMe/GroupCreationViewController.swift
+++ MeetMe/GroupCreationViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class GroupCreationViewController: UIViewController {
 
@@ -16,6 +17,8 @@ class GroupCreationViewController: UIViewController {
     
     var delegate: UIViewController!
     var newGroup: Group = Group()
+    
+    let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +56,52 @@ class GroupCreationViewController: UIViewController {
             newGroup.groupDescr = groupDescriptionTextField.text!
             let otherVC = delegate as! GroupsPage
             otherVC.addGroup(newGroup: newGroup)
+            if Auth.auth().currentUser != nil {
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    let uid = user.uid
+//                    let nameRef = db.collection("Users").document(uid)
+                    
+//                    nameRef.getDocument { (document, error) in
+//                        if let document = document, document.exists {
+//                            let data = document.data()
+//                            let name = data!["name"] as? String ?? ""
+//
+//                        } else {
+//                            print("Document does not exist")
+//                        }
+//                    }
+                    // Create hash of the groups object
+                    var hasher = Hasher()
+                    hasher.combine(newGroup.groupName)
+                    hasher.combine(newGroup.groupDescr)
+                    let hash = String(hasher.finalize())
+                    // Create the instance object
+                    let groupDb : [String: Any] = [
+                        "uid": hash,
+                        "name": newGroup.groupName,
+                        "admin": true,
+                        "whoCreated": uid,
+                        "description": newGroup.groupDescr,
+                        "inviteLink": "",
+                        "peopleInGroup": [],
+                        "events": []
+                    ]
+                    // Add it to the groups instance
+                    self.db.collection("Groups").document(hash).setData(groupDb)
+                    // Search for the user and append it to existing array
+                    self.db.collection("Users").document(uid).updateData(["groupsAll": FieldValue.arrayUnion([hash])])
+                    
+                    
+                    
+                    
+                }
+                
+                
+            } else {
+              // No user is signed in.
+              // ...
+            }
         } else {
             let controller = UIAlertController(
                 title: "Missing Name",
