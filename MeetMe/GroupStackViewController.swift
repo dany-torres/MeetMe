ba9308c309bc4501fe21a348d56ffc3216822c08
dany-torres <53352534+dany-torres@users.noreply.github.com>
2021-11-24 11:48:02 -0600

    Added stack implementation

--- MeetMe/GroupStackViewController.swift
+++ MeetMe/GroupStackViewController.swift
@@ -13,25 +13,33 @@ protocol AddNewEvent {
 }
 
 class GroupStackViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AddNewEvent {
-    public var eventList:[Event] = []
-    var delegate: UITableView!
-    var currGroupHASH : String!
-    var currGroupName : String!
     
     let db = Firestore.firestore()
     
     @IBOutlet weak var eventStack: UITableView!
+    @IBOutlet weak var dateLabel: UILabel!
     
+    public var eventList:[Event] = []
+    var delegate: UITableView!
+    var currGroupHASH : String!
+    var currGroupName : String!
+    var halfHours:[String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Change back button ot go to root rather than create group
         var viewControllersArray = [UIViewController]()
         viewControllersArray.append(self.navigationController!.viewControllers.first!)
         viewControllersArray.append(self.navigationController!.viewControllers.last!)
         self.navigationController?.setViewControllers(viewControllersArray, animated: false)
-//        currGroupHASH = dummy
+        
         eventStack.delegate = self
         eventStack.dataSource = self
+        
+        setDayLabel()
+        initTime()
+        
         let queue = DispatchQueue(label: "curr")
         queue.async {
             while (self.currGroupHASH == nil){
@@ -39,21 +47,144 @@ class GroupStackViewController: UIViewController, UITableViewDataSource, UITable
             }
             self.rePopulateEventStack()
         }
-        // Do any additional setup after loading the view.
     }
+    
+    // Initialize the halfHours array
+    func initTime(){
+        let possibleHours = ["00", "30"];
+        for j in 0...1 {
+            if j == 0{
+                halfHours.append("12:00 AM");
+                halfHours.append("12:30 AM");
+            } else {
+                halfHours.append("12:00 PM");
+                halfHours.append("12:30 PM");
+            }
+            for i in 1...11 {
+                var hourString = String()
+                for item in possibleHours{
+                    hourString = String(i) + ":" + item
+                    if j == 0{
+                        halfHours.append(hourString + " AM");
+                    } else {
+                        halfHours.append(hourString + " PM");
+                    }
+                }
+            }
+        }
+    }
+    
+    // Sets the dat label to the current day
+    func setDayLabel(){
+        let today = Date()
+        let weekday = Calendar.current.component(.weekday, from: today)
+        let month = Calendar.current.component(.month, from: today)
+        let year =
+        Calendar.current.component(.year, from: today)
+        let date = Calendar.current.component(.day, from: today)
 
+        let weekdayText = Calendar.current.shortWeekdaySymbols[weekday-1]
+        let monthText = "\(Calendar.current.shortMonthSymbols[month-1]) \(date)"
+        
+        dateLabel.text = "\(weekdayText) \(monthText), \(year)"
+        eventStack.reloadData()
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        eventList.count
+        halfHours.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "stackCell", for: indexPath)
-        
-        //After creating the cell, update the properties of the cell with appropriate data values.
-        let row = indexPath.row
-        cell.textLabel?.text = eventList[row].printEventDetails()
-        return cell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "stackCell", for: indexPath) as! StackTableViewCell
+            let row = indexPath.row
+            let time = halfHours[row]
+            cell.time.text = time
+            let events = getEventsAtCellTime(startTime: time)
+            setEvents(cell:cell, events:events)
+            return cell
     }
+    
+    // Set the events block in the cells accordingly
+    func setEvents(cell:StackTableViewCell, events:[Event]) {
+        hideUnusedEvents(cell:cell)
+        switch events.count {
+        case 1:
+            setFirstEventBlock(cell:cell, event:events[0])
+        case 2:
+            setFirstEventBlock(cell:cell, event:events[0])
+            setSecondEventBlock(cell:cell, event:events[1])
+        case 3:
+            setFirstEventBlock(cell:cell, event:events[0])
+            setSecondEventBlock(cell:cell, event:events[1])
+            setThirdEventBlock(cell:cell, event:events[2])
+        default:
+            if events.count > 3 {
+                setFirstEventBlock(cell:cell, event:events[0])
+                setSecondEventBlock(cell:cell, event:events[1])
+                setMoreThanThreeEvents(cell:cell, extraEvents:events.count - 2)
+            }
+            break
+        }
+    }
+    
+    // Set the first event block
+    func setFirstEventBlock(cell:StackTableViewCell, event:Event){
+        cell.event1.isHidden = false
+        cell.event1.text = event.eventName
+    }
+    
+    // Set the second event block
+    func setSecondEventBlock(cell:StackTableViewCell, event:Event){
+        cell.event2.isHidden = false
+        cell.event2.text = event.eventName
+    }
+    
+    // Set the third event block
+    func setThirdEventBlock(cell:StackTableViewCell, event:Event){
+        cell.event3.isHidden = false
+        cell.event3.text = event.eventName
+    }
+    
+    // Set the third event block when there are more events
+    func setMoreThanThreeEvents(cell:StackTableViewCell, extraEvents:Int){
+        cell.event3.isHidden = false
+        cell.event3.text = String(extraEvents) + " More Events"
+    }
+    
+    
+    // Hide any event blocks that are not being used
+    func hideUnusedEvents(cell:StackTableViewCell) {
+        cell.event1.isHidden = true
+        cell.event2.isHidden = true
+        cell.event3.isHidden = true
+    }
+    
+    
+    // Get all events that start at a given time
+    func getEventsAtCellTime(startTime:String) -> [Event]{
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        var eventsAtTime = [Event]()
+        for event in eventList {
+            let start = dateFormatter.date(from: event.startTime)
+            let end = dateFormatter.date(from: event.endTime)
+            let startTimeDate = dateFormatter.date(from: startTime)
+            
+            if event.startTime == startTime {
+                eventsAtTime.append(event)
+            }
+            
+            if start! < startTimeDate! && end! > startTimeDate! {
+                eventsAtTime.append(event)
+                // TODO: fix so that it looks like one event box
+            }
+        }
+        
+        return eventsAtTime
+    }
+    
     
 
     // MARK: - Navigation
