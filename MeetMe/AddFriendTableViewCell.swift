b53bc8b3e1fbd8ab7f0ce1a146565ff8431a857d
Alejandro Balderas <alejandro_balderas@utexas.edu>
2021-11-30 08:56:56 -0600

    add friends half implemented

---
+++ MeetMe/AddFriendTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AddFriendTableViewCell.swift
+//  MeetMe
+//
+//  Created by Alejandro Balderas Knoell on 11/30/21.
+//
+
+import UIKit
+
+class AddFriendTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
