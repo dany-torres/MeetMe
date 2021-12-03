e6a7c70c638a7553464aa0af6cfd01fa85c55c11
Pamela Vazquez <pamvazquez1@gmail.com>
2021-12-03 14:31:37 -0600

    Added full code for button logic in search VC, need to test

--- MeetMe/SearchViewController.swift
+++ MeetMe/SearchViewController.swift
@@ -87,41 +87,106 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: UserCellIdentifier, for: indexPath) as! UserRequestTableViewCell
         let row = indexPath.row
-        let user = usersList[row]
-        cell.nameLabel.text = user.name
-        cell.usernameLabel.text = user.username
+        let searchedUser = usersList[row]
+        cell.nameLabel.text = searchedUser.name
+        cell.usernameLabel.text = searchedUser.username
+        
+        //check cases and update button label
+        //case 1: searched user is in friendreq list && not in friends list -> button label: requested
+        //case 2: searched user is not in friendreq list && not in friends list -> button label: add
+        //case 3: searched user is not in friendreq list and in friends list -> button label: friends
+        
+        //search for users 
+        db.collection("Users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    if Auth.auth().currentUser != nil {
+                        let user = Auth.auth().currentUser
+                        if let user = user {
+                            let uid = user.uid
+                            let nameRef = self.db.collection("Users").document(uid)
+                            nameRef.getDocument { (document, error) in
+                                if let document = document, document.exists {
+                                    let data = document.data()
+                                    let friends = data!["friends"] as! [String]
+                                    let friendRequests = data!["friendRequests"] as! [String]
+                                    //friends.append(uid)
+                                    if (friendRequests.contains(searchedUser.hash) && !friends.contains(searchedUser.hash)) {
+                                        cell.requestButton.setTitle("Requested", for: .normal)
+                                        self.resultsTableView.reloadData()
+                                    }
+                                    if(!friendRequests.contains(searchedUser.hash) && !friends.contains(searchedUser.hash)){
+                                        cell.requestButton.setTitle("Add", for: .normal)
+                                        self.resultsTableView.reloadData()
+                                    }
+                                    if(!friendRequests.contains(searchedUser.hash) && friends.contains(searchedUser.hash)){
+                                        cell.requestButton.setTitle("Added", for: .normal)
+                                        self.resultsTableView.reloadData()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
         return cell
     }
     
     //when button is clicked, add the uid of curr user to clicked users friend request list
+    //when loading the table view, we want to set the title of the button based on wether the searched user is in
+    //the currents friendrequest list or friend list
+    //case 1: searched user is in friendreq list && not in friends list -> button label: requested
+    //case 2: searched user is not in friendreq list && not in friends list -> button label: add
+    //case 3: searched user is not in friendreq list and in friends list -> button label: friends
+    
     func didTapButton(cell: UserRequestTableViewCell) {
         
         //Get the indexpath of cell where button was tapped
         let indexPath = self.resultsTableView.indexPath(for: cell)
         let cell = resultsTableView.cellForRow(at: indexPath!) as! UserRequestTableViewCell
         
-        //change button status
-        cell.requestButton.setTitle("Request Sent", for: .normal)
+        //check status of button
+        let buttonStatus = cell.requestButton.titleLabel?.text
         
-        currCell = cell
-                
-        let row = (indexPath?.row)!
-        
-        let newFriendRequest = usersList[row]
-        let newFriendRequestHash = newFriendRequest.hash
-        
-        if Auth.auth().currentUser != nil {
+        if(buttonStatus == "Add"){
             
-            //get current user
-            let user = Auth.auth().currentUser
-            if let user = user {
-                let uid = user.uid
+            //change button status
+            cell.requestButton.setTitle("Requested", for: .normal)
                 
-                // Search for the curr user and add the uid of curr user to friend request list of clicked user
-                self.db.collection("Users").document(newFriendRequestHash).updateData(["friendRequests": FieldValue.arrayUnion([uid])])
+            currCell = cell
+                    
+            let row = (indexPath?.row)!
+            
+            let newFriendRequest = usersList[row]
+            let newFriendRequestHash = newFriendRequest.hash
+            
+            if Auth.auth().currentUser != nil {
                 
-                
+                //get current user
+                let user = Auth.auth().currentUser
+                if let user = user {
+                    let uid = user.uid
+                    
+                    
+                    // Search for the curr user and add the uid of curr user to friend request list of clicked user
+                    self.db.collection("Users").document(newFriendRequestHash).updateData(["friendRequests": FieldValue.arrayUnion([uid])])
+                    
+                    //also remove it from user list or ??
+                    
+                    
+                }
             }
+            
+        }
+        
+        //else there is nothing to do but wait until the other person accepts the request 
+                
+         
+        
+        
     }
-}
 }
